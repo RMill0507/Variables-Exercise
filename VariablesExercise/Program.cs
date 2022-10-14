e49eb1da3d5fdc098a1f130dbe7fb751d08c3f30
Rob <Rmilla445@gmail.com>
2022-10-14 12:12:37 -0400

    Declare and Initialize Variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        //Variable Declarations
+        int age;
+        string name;
+        bool isAlive;
+        char myCharacter;
+        decimal myDecimal;
+        double myDouble;
+
+        //Variable Initializations
+        age = 30;
+        name = "Rob";
+        isAlive = false;
+        myCharacter = 'a';
+        myDecimal = 4485.3454m;
+        myDouble = 45.5;
+
+
+            Console.WriteLine($"My name is {name}, I am {age} years old");
+            Console.ReadLine();
+            
         }
     }
 }
